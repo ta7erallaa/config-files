f9a33006257b608a0bb7102e17b9a40ba296ab89
taheralala <ta7ermo2002@gmail.com>
2025-10-28 19:35:24 +0300

    make temeplate task that contain the most used property to use them latter with another task

--- mise.toml.go
+++ mise.toml.go
@@ -1,5 +1,5 @@
 [tools]
-go = "1.24.5"
+go = ""
 
 [vars]
 BUILD_DIR = "bin"
@@ -9,20 +9,19 @@ BUILD_FLAG = '-ldflags="-s -w -X main.Version={{vars.VERSION}}"'
 GO = "go"
 GOFLAGS = "-ldflags='-s -w'"
 
-[tasks.build]
-description = "Build app_name"
-shell = "bash -c"
-alias = "b"
-run = """
-mkdir -p {{vars.BUILD_DIR}}
-{{vars.GO}} build {{vars.BUILD_FLAG}} -o {{vars.BUILD_DIR}}/{{vars.BINARY_NAME}}
-"""
-sources = ["./*.go"]
-outputs = ["{{vars.BUILD_DIR}}/{{vars.BINARY_NAME}}"]
+[tasks.template]
+description =""
+shell =""
+alias =""
+run ="""""" // can be []
+sources = []
+outputs = []
 quiet = true // hide task running command.
 
-[tasks.run]
-description = "Run {{vars.BINARY_NAME}}"
-run = "./{{vars.BUILD_DIR}}/{{vars.BINARY_NAME}}"
-depends = ["build"]
-alias = "r"
+// if you need arg, flag, etc for task
+usage = '''
+arg "<env>" default="value"{
+	choises "n" "n2"
+}
+flag = ""
+'''
